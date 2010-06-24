2a4efa8b2723ff54b799a38f400bb2da795ac570
mikesname <mikesname@localhost>
2010-06-24 10:52:50 +0100

    Add app to do generic image tranforms

--- urls.py
+++ urls.py
@@ -9,12 +9,15 @@ urlpatterns = patterns('',
     # Static media
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_ROOT}),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
     
     (r'^/?$', include('ocradmin.ocr.urls')),
     (r'^ocr/', include('ocradmin.ocr.urls')),
     (r'^accounts/?', include('ocradmin.accounts.urls')),
     (r'^ocrtasks/', include('ocradmin.ocrtasks.urls')),
     (r'^ocrmodels/', include('ocradmin.ocrmodels.urls')),
+    (r'^imageops/', include('ocradmin.imageops.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
