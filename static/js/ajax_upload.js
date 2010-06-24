89af406c5b2f928d9574704f3a7aed50fbe2e4e1
mikesname <mikesname@localhost>
2010-06-24 10:53:38 +0100

    Moved some static stuff into its own file.  Added test image

---
+++ static/js/ajax_upload.js
@@ -0,0 +1,8 @@
+$(function() {
+    var XHRQUEUE = [];
+    var BOUNDARY = '------multipartformboundary' + (new Date).getTime();
+    var DASHDASH = '--';
+    var CRLF     = '\r\n';
+    var NUMFILES = 1;
+
+});
