f48ec237a1980acc3c12ee8d134fcfe8e093d012
mikesname <mikesname@localhost>
2010-07-07 17:48:31 +0100

    Refactoring (which hopefully didn't break anything.)

--- static/js/ocr_viewing_common.js
+++ static/js/ocr_viewing_common.js
@@ -54,16 +54,145 @@ window.onbeforeunload = function(event) {
 
 
 
+function refreshImage() {
+    sdviewer.setWaiting(true);
+
+    var params = "src=" + $("#viewerwindow").data("src") 
+        + "&png=" + $("#viewerwindow").data("png") 
+        + "&dst=" + sdviewer.activeOutputPath()
+        + "&" + $("#optionsform").serialize();
+
+    $.ajax({
+        url: window.location.pathname,
+        data: params, 
+        type: "POST",
+        dataType: "json",
+        beforeSend: function(data) {
+            $(".compparam > input, .ocroption").attr("disabled", true);
+        },
+        success: function(data) {
+            $("#viewerwindow").data("jobname", data[0].job_name);
+            pollForResults($("#viewerwindow"));
+        },
+        error: function(xhr, errorResponse, errorThrown) {
+            alert("XHR failed: " + errorResponse);
+        },
+        complete: function(e) {
+        }
+    });
+}
+
+// Process the data completed results data... in this case
+// set the viewer source and output paths
+function processData(element, data) {
+    if (!data || !data.status || data.status == "PENDING") {
+        setTimeout(function() {
+            pollForResults(element);
+            }, 500);
+    } else if (data.error) {
+        sdviewer.setWaiting(true);
+        element
+            .addClass("error")
+            .html("<h4>Error: " + data.error + "</h4>")
+            .append(
+                $("<div></div>").addClass("traceback")
+                    .append("<pre>" + data.trace + "</pre>")                                
+            );                            
+    } else if (data.status == "SUCCESS") {
+        element.data("png", data.results.png);
+        element.data("src", data.results.src);
+        if (data.results.dst.search("_b.dzi") != -1) {
+            element.data("outb", data.results.dst);
+            //sdviewer.setOutputB(data.results.dst + "?" + (new Date().getTime()));
+        } else {
+            element.data("outa", data.results.dst);
+            //sdviewer.setOutputA(data.results.dst + "?" + (new Date().getTime()));
+        }
+//            alert("set primary buffer: " + data.results.dst);
+        sdviewer.setOutput(data.results.dst);
+        sdviewer.setSource(data.results.src);
+        sdviewer.setWaiting(false);
+        $(".compparam > input, .ocroption").attr("disabled", false);
+        $(".interact_param").attr("disabled", false);
+    } else {
+        alert("Oops.  Task finished with bad status: " + data.status);
+    }
+} 
+            
+// keep checking the server for the results of the jobname
+// associated with the particular element
+function pollForResults(element) {
+    var jobname = element.data("jobname");
+    $.ajax({
+        url: "/ocr/results/" + jobname,
+        dataType: "json",
+        success: function(data) {
+            processData(element, data);    
+        },
+        error: function(xhr, statusText, errorThrown) {
+            var msg = "Http Error: " + statusText;
+            if (errorThrown) {
+                msg += "\n\n" + errorThrown;
+            }
+            alert(msg);
+        }
+    }); 
+}
+
+// process the INITIAL json data that arrives after the files
+// have been uploaded but before it has been processed.
+function onXHRLoad(event_or_response) {
+    if (event_or_response.target != null) {
+        var xhr = event_or_response.target;
+        if (!xhr.responseText) {
+            return;
+        }                
+        if (xhr.status != 200) {
+            return alert("Error: " + xhr.responseText + "  Status: " + xhr.status);
+        }        
+        data = $.parseJSON(xhr.responseText);
+    } else {
+        // then it must be a single upload...
+        data = event_or_response;
+    }
+    if (data.error) {
+        alert("Error: " + data.error + "\n\n" + data.trace);
+        $("#dropzone").text("Drop images here...").removeClass("waiting");
+        return;
+    }
+
+    $.each(data, function(page, pageresults) {
+        var pagename = pageresults.job_name.split("::")[0].replace(/\.[^\.]+$/, "");
+        sdviewer.setTitle(pagename);
+        sdviewer.setWaiting(true);
+        $("#viewerwindow")
+            .data("jobname", pageresults.job_name);
+        pollForResults($("#viewerwindow"));
+    }); 
+};
+
+
+
 $(function() {
 
     // initialise the viewer
     sdviewer = new ImageWindow("viewerwindow"); 
     sdviewer.init();
-    
-   // ajaxify the upload form
-   $("#uploadform").ajaxForm({
+
+    // refrwesh on compparm enter
+    $(".compparam > input").live("keydown", function(event) {
+        if (event.keyCode == 13) {
+            refreshImage();            
+        }
+    });
+
+    // ajaxify the upload form
+    $("#uploadform").ajaxForm({
         data : { _iframe: 1 },
         dataType: "json",
+        beforeSend: function(e) {
+            $(".compparam > input, .ocroption").attr("disabled", true);
+        },
         success: function(data, responseText, xhr) {
             onXHRLoad(data, responseText, xhr);
             $("#singleupload").val("");
@@ -88,94 +217,6 @@ $(function() {
     // make interactive params disabled at the start
     $(".interact_param").attr("disabled", true);
 
-    // Process the data completed results data... in this case
-    // set the viewer source and output paths
-    function processData(element, data) {
-        if (!data || !data.status || data.status == "PENDING") {
-            setTimeout(function() {
-                pollForResults(element);
-                }, 500);
-        } else if (data.error) {
-            sdviewer.setWaiting(true);
-            element
-                .addClass("error")
-                .html("<h4>Error: " + data.error + "</h4>")
-                .append(
-                    $("<div></div>").addClass("traceback")
-                        .append("<pre>" + data.trace + "</pre>")                                
-                );                            
-        } else if (data.status == "SUCCESS") {
-            element.data("png", data.results.png);
-            element.data("src", data.results.src);
-            if (data.results.dst.search("_b.dzi") != -1) {
-                element.data("outb", data.results.dst);
-                //sdviewer.setOutputB(data.results.dst + "?" + (new Date().getTime()));
-            } else {
-                element.data("outa", data.results.dst);
-                //sdviewer.setOutputA(data.results.dst + "?" + (new Date().getTime()));
-            }
-//            alert("set primary buffer: " + data.results.dst);
-            sdviewer.setOutput(data.results.dst);
-            sdviewer.setSource(data.results.src);
-            sdviewer.setWaiting(false);
-            $(".interact_param").attr("disabled", false);
-        } else {
-            alert("Oops.  Task finished with bad status: " + data.status);
-        }
-    } 
-                
-    // keep checking the server for the results of the jobname
-    // associated with the particular element
-    function pollForResults(element) {
-        var jobname = element.data("jobname");
-        $.ajax({
-            url: "/ocr/results/" + jobname,
-            dataType: "json",
-            success: function(data) {
-                processData(element, data);    
-            },
-            error: function(xhr, statusText, errorThrown) {
-                var msg = "Http Error: " + statusText;
-                if (errorThrown) {
-                    msg += "\n\n" + errorThrown;
-                }
-                alert(msg);
-            }
-        }); 
-    }
-
-    // process the INITIAL json data that arrives after the files
-    // have been uploaded but before it has been processed.
-    function onXHRLoad(event_or_response) {
-        if (event_or_response.target != null) {
-            var xhr = event_or_response.target;
-            if (!xhr.responseText) {
-                return;
-            }                
-            if (xhr.status != 200) {
-                return alert("Error: " + xhr.responseText + "  Status: " + xhr.status);
-            } 
-            data = $.parseJSON(xhr.responseText);
-        } else {
-            // then it must be a single upload...
-            data = event_or_response;
-        }
-
-        if (data.error) {
-            alert("Error: " + data.error + "\n\n" + data.trace);
-            $("#dropzone").text("Drop images here...").removeClass("waiting");
-            return;
-        }
-
-        $.each(data, function(page, pageresults) {
-            var pagename = pageresults.job_name.split("::")[0].replace(/\.[^\.]+$/, "");
-            sdviewer.setTitle(pagename);
-            sdviewer.setWaiting(true);
-            $("#viewerwindow")
-                .data("jobname", pageresults.job_name);
-            pollForResults($("#viewerwindow"));
-        }); 
-    };
 
 
     // toggle the source and binary images
@@ -188,33 +229,7 @@ $(function() {
 
 
     // resubmit the form...
-    $("#refresh").click(function(e) {
-        // check if we need to crop the re-binarized section
-        if ($("#cropsize").attr("checked")) {
-                        
-        }    
-
-
-        sdviewer.setWaiting(true);
-        var params = "src=" + $("#viewerwindow").data("src") 
-            + "&png=" + $("#viewerwindow").data("png") 
-            + "&dst=" + sdviewer.activeOutputPath()
-            + "&" + $("#optionsform").serialize();
-        $.ajax({
-            url: window.location.pathname,
-            type: "POST",
-            data: params, 
-            dataType: "text",
-            success: function(data) {
-                data = $.parseJSON(data);
-                $("#viewerwindow").data("jobname", data[0].job_name);
-                pollForResults($("#viewerwindow"));
-            },
-            error: function(xhr, errorResponse, errorThrown) {
-                alert("XHR failed: " + errorResponse);
-            }
-        });
-    });
+    $("#refresh").click(refreshImage);
 
     // initialise the controls
     buildComponentOptions();
@@ -244,6 +259,7 @@ $(function() {
     uploader.onUploadsStarted = function(e) {
         // close anything that's already open in the viewer
         sdviewer.close();
+        $(".compparam > input, .ocroption").attr("disabled", true);
         
         // slurp up the parameters.  Since the params are build 
         // dynamically this has to be done immediately before the
