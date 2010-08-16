26e86503e25d478a70cf4a0b1a0db8a84748546f
mikesname <mikesname@localhost>
2010-08-16 15:58:09 +0100

    Switch on the projects app

--- settings.py
+++ settings.py
@@ -147,6 +147,7 @@ INSTALLED_APPS = (
     'ocrmodels',
     'ocrpresets',
     'ocrtasks',
+    'projects',
     'tagging',
 )
 
--- urls.py
+++ urls.py
@@ -20,6 +20,7 @@ urlpatterns = patterns('',
     (r'^ocrtasks/?', include('ocradmin.ocrtasks.urls')),
     (r'^ocrmodels/?', include('ocradmin.ocrmodels.urls')),
     (r'^ocrpresets/?', include('ocradmin.ocrpresets.urls')),
+    (r'^projects/?', include('ocradmin.projects.urls')),
     (r'^imageops/?', include('ocradmin.imageops.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
