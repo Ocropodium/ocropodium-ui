2dcbfea3d29b843f3531862d6990861251b8e40b
mikesname <mikesname@localhost>
2010-09-03 13:19:02 +0100

    Remove constants that have now been moved to a global file

--- static/js/ocr_line_editor.js
+++ static/js/ocr_line_editor.js
@@ -5,21 +5,6 @@ function OcrLineEditor(insertinto_id) {
     // the element we're operating on
     var m_elem = null;
 
-    // useful key codes
-    const KC_ESCAPE = 27;
-    const KC_RETURN = 13;
-    const KC_LEFT = 37;
-    const KC_RIGHT = 39;
-    const KC_DELETE = 46;
-    const KC_BACKSPACE = 8;
-    const KC_SHIFT = 16;
-    const KC_CTRL = 17;
-    const KC_ALT = 18;
-    const KC_CAPSLOCK = 20;
-    const KC_TAB = 9;
-    const KC_END = 35;
-    const KC_HOME = 36;
-
     const LONGKEY = 500;
     const SHORTKEY = 70;
 
