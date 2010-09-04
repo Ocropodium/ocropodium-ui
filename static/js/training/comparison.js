0653871137a4747be2b1d479dd64bee99a94be2f
mikesname <mikesname@localhost>
2010-09-04 18:10:53 +0100

    Make the training stuff prettier

--- static/js/training/comparison.js
+++ static/js/training/comparison.js
@@ -1,14 +1,17 @@
 // JS for comparison.html
 
 function refreshData() {
-    if ($("h3.inprogress").length) {
+    if ($(".inprogress").length) {
         setTimeout(function() {
-            $(".widget_content").load(window.location.pathname);
+            $(".widget_content").load(window.location.pathname, function() {
+                $("#tabs").tabs();
+            });
             refreshData();
         }, 1000);
     }
 }
 
 $(function() {
+    $("#tabs").tabs();
     refreshData();    
 });
