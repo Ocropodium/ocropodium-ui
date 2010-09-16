bc4929fe9a464fdb7cfeb2de445c727d5c0f9571
mikesname <mikesname@localhost>
2010-09-16 16:35:10 +0100

    Remove old file

--- static/js/ajax_upload.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var XHRQUEUE = [];
-    var BOUNDARY = '------multipartformboundary' + (new Date).getTime();
-    var DASHDASH = '--';
-    var CRLF     = '\r\n';
-    var NUMFILES = 1;
-
-});
