8f754a11f317df4b103347b8c6007fa61ab4bcf6
mikesname <mikesname@localhost>
2010-10-12 15:32:08 +0100

    Added a bit of keyboard nav to the main menu

--- static/js/loadall.js
+++ static/js/loadall.js
@@ -25,37 +25,43 @@ $(function() {
     }
 
     function activateMenu(menu) {
-        $(menu).unbind("click.menuopen");
-        $("div#menu ul.top").not($(menu))
-            .removeClass("selected")
-            .unbind("mouseenter.menuover")
-            .bind("mouseenter.menuover", function(event) {
-                activateMenu(this);
-            }).bind("click.menuopen", function(event) {
-                activateMenu(this);    
-            }).find("ul").hide();
-        $(menu).addClass("selected")
+        $("div#menu ul.top").not($(menu)).each(function(i, elem) {
+            deactivateMenu(elem);
+        }).bind("mouseenter.menuover", function(event) {
+            activateMenu(this);
+        });
+        $(menu)
+            .addClass("selected")
+            .unbind("click.menuopen")
             .bind("click.menuclose", function(event) {
                 deactivateMenu(menu);
             }).find("ul").show();
-        $(window).bind("keydown.menuclose", function(event) {
-            if (event.keyCode == KC_ESCAPE) {
-                deactivateMenu(menu);
-                $("div#menu ul.top").unbind("click.menuclose");
-                $(window).unbind("keydown.menuclose");
-            } 
-        });
-        $(window).bind("click.menuclose", function(event) {
-            if (event.pageX > $(menu).offset().left &&
-                event.pageX < $(menu).offset().left +
-                    $(menu).width() &&
-                event.pageY > $(menu).offset().top &&
-                event.pageY < $(menu).offset().top +
-                    $(menu).height()) {
-                return true;
-            }
-            deactivateMenu(menu);            
-        });
+        $(window)
+            .bind("keydown.menuclose", function(event) {
+                if (event.keyCode == KC_ESCAPE) {
+                    deactivateMenu(menu);
+                    $("div#menu ul.top").unbind("click.menuclose");
+                    $(window).unbind("keydown.menuclose");
+                } else if (event.keyCode == KC_LEFT) {
+                    if (menu.previousElementSibling) {
+                        activateMenu(menu.previousElementSibling);
+                    }
+                } else if (event.keyCode == KC_RIGHT) {
+                    if (menu.nextElementSibling) {
+                        activateMenu(menu.nextElementSibling);
+                    }
+                } 
+            }).bind("click.menuclose", function(event) {
+                if (event.pageX > $(menu).offset().left &&
+                    event.pageX < $(menu).offset().left +
+                        $(menu).width() &&
+                    event.pageY > $(menu).offset().top &&
+                    event.pageY < $(menu).offset().top +
+                        $(menu).height()) {
+                    return true;
+                }
+                deactivateMenu(menu);            
+            });
     }
 
     // active main menu click dropdown
