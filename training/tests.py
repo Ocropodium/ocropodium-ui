073373a973ebd8d4e3f409c7d5a4fab37a568aa1
mikesname <mikesname@localhost>
2010-10-13 11:01:14 +0100

    Add some very basic training app tests.  Not testing anything hard yet.

--- training/tests.py
+++ training/tests.py
@@ -1,23 +1,67 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+    Training tests.
 """
-
+import os
+import re
+import shutil
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils import simplejson
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+
+TESTFILE = "simple.png"
+
+
+class TrainingTest(TestCase):
+    fixtures = ["ocrmodels/fixtures/test_fixtures.json",
+            "projects/fixtures/test_fixtures.json"]
+
+    def setUp(self):
         """
-        Tests that 1 + 1 always equals 2.
+            Setup OCR tests.  Creates a test user.
         """
-        self.failUnlessEqual(1 + 1, 2)
+        #shutil.copy2("media/models/mytessdata.tgz", "media/test/engtessdata.tgz")
+        #shutil.copy2("media/models/default.model", "media/test/default.model")
+        #shutil.copy2("media/models/default.fst", "media/test/default.fst")
+        #os.makedirs("media/files/test_user/test")
+        #shutil.copy2("media/test/%s" % TESTFILE, "media/files/test_user/test/%s" % TESTFILE)
+        self.testuser = User.objects.create_user("test_user", "test@testing.com", "testpass")
+        self.client = Client()
+        self.client.login(username="test_user", password="testpass")
+        self.client.get("/projects/load/1/")
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+    def tearDown(self):
+        """
+            Cleanup a test.
+        """
+        self.testuser.delete()
+        #shutil.rmtree("media/files/test_user")
+
+    def test_new_view(self):
+        """
+        Test viewing the training submission form.
+        """
+        r = self.client.get("/training/new/")
+        self.assertEqual(r.status_code, 200)
+
+
+    def test_list_view(self):
+        """
+        Test viewing the training set list.
+        """
+        r = self.client.get("/training/list/")
+        self.assertEqual(r.status_code, 200)
+
+
+    def test_comparisons_view(self):
+        """
+        Test viewing the comparisons list.
+        """
+        r = self.client.get("/training/comparisons/")
+        self.assertEqual(r.status_code, 200)
+
+
 
