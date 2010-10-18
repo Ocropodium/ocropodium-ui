53f72a12714588f133aa624798dcb33a7ac648e8
mikesname <mikesname@localhost>
2010-10-18 15:01:09 +0100

    Remove defunk JS from new project

--- static/js/projects/new.js
+++ static/js/projects/new.js
@@ -3,21 +3,4 @@ $(function() {
     $("#id_name").bind("keyup", function(event) {
         validateProjectForm();
     });
-
-    $("legend.collapsed").next().hide();
-    $("legend.collapsed, legend.expanded").click(function(e) {
-        if ($(this).hasClass("collapsed")) {
-            $(this)
-                .removeClass("collapsed")
-                .addClass("expanded")
-                .next().show();
-        } else {
-            $(this)
-                .removeClass("expanded")
-                .addClass("collapsed")
-                .next().hide();
-        }
-    });
-
-
 });
