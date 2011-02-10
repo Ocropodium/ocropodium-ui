f0b57e34f8c174a2b8aacafb0de886757548b610
mikesname <mikesname@localhost>
2011-02-10 17:17:51 +0000

    Added app for quering plugin configuation and test pages to experiment with building front-end

---
+++ ocradmin/ocrplugins/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^/?$', 'ocradmin.ocrplugins.views.list'),
+    (r'^query/?$', 'ocradmin.ocrplugins.views.query'),
+    (r'^query/(?P<args>.+)/?$', 'ocradmin.ocrplugins.views.query'),
+	(r'^(?P<name>[^/]+)/?$', 'ocradmin.ocrplugins.views.info'),
+	(r'^(?P<name>\w+)/(?P<method>\w+)/?$', 'ocradmin.ocrplugins.views.run_get_method'),
+)
