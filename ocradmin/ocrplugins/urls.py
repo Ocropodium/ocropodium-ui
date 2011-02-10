f21af20637c0181b9f8c7d9d29e8afb0581c6fe5
mikesname <mikesname@localhost>
2011-02-10 21:56:50 +0000

    Slightly less breakage, still broken.  Ah, the things you can get away with checking in when working alone...

--- ocradmin/ocrplugins/urls.py
+++ ocradmin/ocrplugins/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('',
     (r'^/?$', 'ocradmin.ocrplugins.views.list'),
     (r'^query/?$', 'ocradmin.ocrplugins.views.query'),
     (r'^query/(?P<args>.+)/?$', 'ocradmin.ocrplugins.views.query'),
+    (r'^parse/?$', 'ocradmin.ocrplugins.views.parse'),
 	(r'^(?P<name>[^/]+)/?$', 'ocradmin.ocrplugins.views.info'),
 	(r'^(?P<name>\w+)/(?P<method>\w+)/?$', 'ocradmin.ocrplugins.views.run_get_method'),
 )
