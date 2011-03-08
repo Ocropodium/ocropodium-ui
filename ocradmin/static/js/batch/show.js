6c32332e0c0f3e3f07f408895de7e60b4e850280
mikesname <mikesname@localhost>
2011-03-08 11:07:33 +0000

    Cleanup.  Put globals in a nested scope.  Now remembers sidebar tab

--- ocradmin/static/js/batch/show.js
+++ ocradmin/static/js/batch/show.js
@@ -1,112 +1,115 @@
-var batch = null;
-var sideparams = null;
-var selectedtab = 0;
-
-
-$(".recent_batch_link").live("click", function(event) {
-    batch.setBatchId($(this).data("pk"));    
-    event.preventDefault();
-});
-
-
-function loadBatchList() {
-    $.ajax({
-        url: "/batch/list?order_by=-created_on",
-        data: {},
-        dataType: "json",
-        error: OCRJS.ajaxErrorHandler,
-        success: function(data) {
-            populateBatchList(data);
-            $(".widget_header", $("#sidebar")).text("Recent Batches");
-        },
-    });
-}
-
-
-function populateBatchList(data) {
-    var list = $("<div></div>"); //$("#recent_batches");
-    var tbatch = $("<div></div>").addClass("recent_batch");
-    var titem = $("<span></span>"); 
-    var tlink = $("<a></a>").addClass("recent_batch_link");
-    var ttran = $("<a></a>").addClass("button");
-    $.each(data.object_list, function(i, batch) {
-        var span = titem.clone();
-        var link = tlink.clone()
-            .attr("href", "/batch/show/" + batch.pk + "/")
-            .data("pk", batch.pk)
-            .text(batch.fields.name);
-        var trans = ttran.clone()
-            .attr("href", "/batch/transcript/" + batch.pk + "/")
-            .css("float", "right")
-            .data("pk", batch.pk)
-            .text("Transcript");
-        list
-            .append(tbatch.clone().append(span.append(trans).append(link)))
-            .textOverflow("...");        
-    });
-    $("#recent_batches").html(list.html());
-}
-
-
-function hashNavigate() {
-    if (document.location.hash.match(/^(#task(\d+))/)) {
-        var taskid = RegExp.$1;
-        var taskpk = RegExp.$2;
-        var sel = $(taskid);
-        var pk = $("#recent_batches").data("task_pk");
-        if (sel.length && pk != taskpk)
-            sel.click();
-    }
-}
-
-
-function loadTaskDetails(index, pk) {
-    var elem = $("#recent_batches");
-    $.ajax({
-        url: "/ocrtasks/show/" + pk + "/",
-        type: "get",
-        success: function(data) {
-            var html = $(data);
-            elem.data("task_pk", pk);
-            var loaded = false;
-            $.getJSON("/ocr/task_config/" + pk + "/", function(data) {
-                sideparams = new OCRJS.ParameterBuilder(
-                        html.find("#options").get(0), data);
-                sideparams.addListeners({
-                    onReadyState: function() {
-                        if (!loaded) {
-                            elem.html(html);
-                            elem.find("#tabs")
-                                .accordion({
-                                    collapsible: true,
-                                    autoHeight: false,
-                                    active: selectedtab,
-                                    change: function(event, ui) {
-                                        selectedtab = ui.options.active; 
-                                    },
-                                });
-                            elem.find("#submit_update_task").attr("disabled", false);
-                            loaded = true;
-                            $(".widget_header", $("#sidebar")).text("Task #" + pk);
-                        }
-                    },
-                    onUpdateStarted: function() {
-                        elem.find("#submit_update_task").attr("disabled", true);
-                    }
-                }).init();
-            });
-        },
-    });
-}
-
-
-
 $(function() {
+
+    var batch = null;
+    var sideparams = null;
+    var selectedtab = $.cookie("selectedtab") || 0;
+    var sidebar = $("#sidebar_content");
+    var header = $(".widget_header", $("#sidebar"));
+
+    //$(".recent_batch_link").live("click", function(event) {
+    //    batch.setBatchId($(this).data("pk"));    
+    //    event.preventDefault();
+    //});
+
+    function loadBatchList() {
+        $.ajax({
+            url: "/batch/list?order_by=-created_on",
+            data: {},
+            dataType: "json",
+            error: OCRJS.ajaxErrorHandler,
+            success: function(data) {
+                populateBatchList(data);
+                header.text("Recent Batches");
+            },
+        });
+    }
+
+    function populateBatchList(data) {
+        var list = $("<div></div>").addClass("recent_batches");
+        var tbatch = $("<div></div>").addClass("recent_batch");
+        var titem = $("<span></span>"); 
+        var tlink = $("<a></a>").addClass("recent_batch_link");
+        var ttran = $("<a></a>").addClass("button");
+        $.each(data.object_list, function(i, batch) {
+            var span = titem.clone();
+            var link = tlink.clone()
+                .attr("href", "/batch/show/" + batch.pk + "/")
+                .data("pk", batch.pk)
+                .text(batch.fields.name);
+            var trans = ttran.clone()
+                .attr("href", "/batch/transcript/" + batch.pk + "/")
+                .css("float", "right")
+                .data("pk", batch.pk)
+                .text("Transcript");
+            list
+                .append(tbatch.clone().append(span.append(trans).append(link)))
+                .textOverflow("...");        
+        });
+        sidebar.html(list);
+    }
+
+    function hashNavigate() {
+        if (document.location.hash.match(/^(#task(\d+))/)) {
+            var taskid = RegExp.$1;
+            var taskpk = RegExp.$2;
+            var sel = $(taskid);
+            var pk = sidebar.data("task_pk");
+            if (sel.length && pk != taskpk)
+                sel.click();
+        }
+    }
+
+
+    function loadTaskDetails(index, pk) {
+        $.ajax({
+            url: "/ocrtasks/show/" + pk + "/",
+            type: "get",
+            success: function(data) {
+                var html = $(data);
+                sidebar.data("task_pk", pk);
+                var loaded = false;
+                $.getJSON("/ocr/task_config/" + pk + "/", function(data) {
+                    sideparams = new OCRJS.ParameterBuilder(
+                            html.find("#options").get(0), data);
+                    sideparams.addListeners({
+                        onReadyState: function() {
+                            if (!loaded) {
+                                sidebar.html(html);
+                                console.log("Active: ", selectedtab);
+                                sidebar.find("#tabs")
+                                    .accordion({
+                                        collapsible: true,
+                                        autoHeight: false,
+                                        active: parseInt(selectedtab),
+                                        change: function(event, ui) {
+                                            selectedtab = ui.options.active;
+                                            $.cookie("selectedtab", selectedtab); 
+                                        },
+                                    });
+                                sidebar.find("#submit_update_task").attr("disabled", false);
+                                loaded = true;
+                                header.text("Task #" + pk);
+                            }
+                        },
+                        onUpdateStarted: function() {
+                            sidebar.find("#submit_update_task").attr("disabled", true);
+                        }
+                    });
+                    sideparams.init();
+                });
+            },
+        });
+    }
+
+
     if ($("#batch_id").length) {
         batch = new OCRJS.BatchWidget($("#workspace").get(0), $("#batch_id").val());
         batch.addListeners({
             onTaskSelected: loadTaskDetails,
-            onTaskDeselected: loadBatchList,
+            onTaskDeselected: function() {
+                window.location.hash = "fifofo";
+                loadBatchList();
+            },
             onUpdate: hashNavigate,                                
         }).init();
 
@@ -117,7 +120,7 @@ $(function() {
         $("#submit_update_task").live("click", function(event) {
             var button = this;
             $(button).attr("disabled", true);
-            var pk = $("#recent_batches").data("task_pk");
+            var pk = sidebar.data("task_pk");
             if (!pk)
                 return;
             $.ajax({
@@ -140,7 +143,6 @@ $(function() {
             loadBatchList();
         }
     }
-
 });        
 
 
