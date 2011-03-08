e93b17131ae4599504050a748fc041b6116bc8cc
mikesname <mikesname@localhost>
2011-03-08 17:17:43 +0000

    Add a simple task for testing handlers, signals, retries etc

--- ocradmin/ocrtasks/tests.py
+++ ocradmin/ocrtasks/tests.py
@@ -9,7 +9,9 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
-
+from ocradmin.core import utils as ocrutils
+from models import OcrTask
+from testutils import TestTask
 
 
 class OcrTaskTest(TestCase):
@@ -42,5 +44,29 @@ class OcrTaskTest(TestCase):
         Test viewing task details.
         """
         self.assertEqual(self.client.get("/ocrtasks/show/1/").status_code, 200)
+
+    def test_generic_task(self):
+        """
+        Test creating a task.
+        """
+        tid = ocrutils.get_new_task_id()
+        args=(4, 5),
+        kwargs = dict(task_id=tid, loglevel=60,)
+        task = OcrTask(
+            task_id=tid,
+            user=self.testuser,
+            page_name="simple.png",
+            task_name=TestTask.name,
+            status="INIT",
+            args=args,
+            kwargs=kwargs,
+        )
+        task.save()
+        async = TestTask.apply_async(*args, **kwargs)
+        async.wait()
+        task = OcrTask.objects.get(task_id=tid)
+        self.assertEqual("SUCCESS", task.status)
+                            
+                            
 
 
