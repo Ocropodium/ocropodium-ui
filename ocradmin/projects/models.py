f85bc1988f1a3ab5fcc92f28042ed6a11f73bde1
mikesname <mikesname@localhost>
2011-03-08 11:51:38 +0000

    Cleaved out everything to do with per-project default settings.  It was prematurely implemented and wouldn't work with the new plugin parameter system

--- ocradmin/projects/models.py
+++ ocradmin/projects/models.py
@@ -2,8 +2,6 @@ import os
 from django.db import models
 from django.contrib.auth.models import User
 from tagging.fields import TagField
-from ocradmin.ocrmodels.models import OcrModel
-from ocradmin.ocrpresets.models import OcrPreset
 
 from ocradmin.core import utils as ocrutils
 
@@ -18,7 +16,6 @@ class OcrProject(models.Model):
     description = models.TextField(blank=True, null=True)
     tags = TagField()
     created_on = models.DateTimeField(auto_now_add=True, editable=False)
-    defaults = models.OneToOneField("OcrProjectDefaults", null=True, blank=True)
 
     def __unicode__(self):
         """
@@ -26,22 +23,3 @@ class OcrProject(models.Model):
         """
         return self.name
 
-class OcrProjectDefaults(models.Model):
-    """
-    OCR Project Defaults.  This is something of a 
-    meta-preset.
-    """
-    lmodel = models.ForeignKey(OcrModel, blank=True, null=True, 
-            related_name="lmodel", limit_choices_to={"type": "lang"})
-    cmodel = models.ForeignKey(OcrModel, blank=True, null=True,
-            related_name="cmodel", limit_choices_to={"type": "char"})
-    binarizer = models.ForeignKey(OcrPreset, blank=True, null=True,
-            related_name="binarizer", limit_choices_to={"type": "binarize"})
-    psegmenter = models.ForeignKey(OcrPreset, blank=True, null=True,
-            related_name="psegmenter", limit_choices_to={"type": "segment"})
-    recognizer = models.ForeignKey(OcrPreset, blank=True, null=True,
-            related_name="recognizer", limit_choices_to={"type": "recognize"})
-
-
-
-    
