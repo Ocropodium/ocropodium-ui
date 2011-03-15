4e079a221452cec04100a589c5001da9ac230bee
mikesname <mikesname@localhost>
2011-03-15 11:32:24 +0000

    Add some dummy parameters to the Abbyy FR wrapper.  They're real options but not yet implemented

--- ocradmin/core/tools/plugins/finereader_wrapper.py
+++ ocradmin/core/tools/plugins/finereader_wrapper.py
@@ -22,6 +22,99 @@ class FinereaderWrapper(generic_wrapper.GenericWrapper):
     binary = get_binary("abbyyocr")
     _parameters = [
         {
+            "name": "preprocessing",
+            "description": "Preprocessing",
+            "parameters": [{
+                    "name": "no_deskew_correction",
+                    "type": "bool",
+                    "description": "Don't correct skew angle",
+                    "value": False,
+                }, {
+                    "name": "invert_image",
+                    "type": "bool",
+                    "description": "Invert image on opening",                    
+                    "value": False,
+                }, {
+                    "name": "mirror_image",
+                    "type": "bool",
+                    "description": "Mirror image on opening",
+                    "value": False,
+                }, {
+                    "name": "no_despeckle",
+                    "type": "bool",
+                    "description": "Don't despeckle on opening",
+                    "value": False,
+                }, {
+                    "name": "image_rotation",
+                    "type": "scalar",
+                    "description": "Rotate image on opening",
+                    "value": "NoRotation",
+                    "choices": [
+                        { "name": "NoRotation", },
+                        { "name": "Clockwise", },
+                        { "name": "CounterClockwise", },
+                    ],
+                },                
+            ],
+        
+        }, {
+            "name": "image_analysis",
+            "description": "Image Analysis Options",
+            "parameters": [
+                {
+                    "name": "detect_inverted",
+                    "description": "Detect inverted image",
+                    "type": "bool",
+                    "value": False,
+                }, {
+                    "name": "detect_orientation",
+                    "description": "Detect image orientation",
+                    "type": "bool",
+                    "value": False,
+                }, {
+                    "name": "dont_detect_pictures",
+                    "desciption": "Don't detect pictures",
+                    "type": "bool",
+                    "value": False,
+                }, {
+                    "name": "dont_detect_inverted",
+                    "description": "Don't detect inverted image",
+                    "type": "bool",
+                    "value": False,
+                }, {
+                    "name": "dont_detect_tables",
+                    "description": "Don't detect tables",
+                    "type": "bool",
+                    "value": False,
+                },
+            ],            
+        }, {
+            "name": "recogniser",
+            "description": "Recogniser Options",
+            "parameters": [
+                {
+                    "name": "balanced_mode",
+                    "description": "Balanced Recognition Mode",
+                    "type": "bool",
+                    "value": False,
+                }, {
+                    "name": "fast_mode",
+                    "description": "Fast Recognition Mode",
+                    "type": "bool",
+                    "value": False,
+                }, {
+                    "name": "case_mode",
+                    "description": "Case Recognition Mode",
+                    "type": "scalar",
+                    "value": "AutoCase",
+                    "choices": [
+                        { "name": "AutoCase", },
+                        { "name": "SmallCase", },
+                        { "name": "CapitalCase", },
+                    ],
+                },
+            ],
+        }, {
             "name": "page_segmentation",
             "description": "Page Segmentation Options",
             "choices": [
