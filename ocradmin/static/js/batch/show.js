2436dde0af647caa29e32ae45c56b5876517398c
mikesname <mikesname@localhost>
2011-05-10 17:05:31 +0100

    Start bringing sanity to batch view/show.  Starting with rendering most of the widget in a Django template and then updating the task info on-demand via Ajax

--- ocradmin/static/js/batch/show.js
+++ ocradmin/static/js/batch/show.js
@@ -101,6 +101,98 @@ $(function() {
         });
     }
 
+    var scrolltimer = -1;
+
+    $("#batchcontainer").resizable({
+        minHeight: $("#batchcontainer").height() - $(".tl_container", this).height(),
+        resize: function(event, ui) {
+            var outer = $(this), inner = $(".tl_container", this);
+            var possdiff = inner.offset().top - (outer.offset().top
+                    - outer.css("marginTop").replace(/px/, ""));
+            inner.outerHeight(outer.height() - possdiff);
+            refreshTasks($(".tl_container", this));
+        }        
+    });
+
+    function topVisibleTask(scroll) {
+        var top = $(scroll).scrollTop(),
+            tasks = $(".batch_task", scroll),
+            margin = parseInt(tasks.css("marginBottom").replace(/px/, ""));
+        return Math.floor(top / (tasks.outerHeight() + margin));
+    }
+
+    function visibleTaskCount(scroll) {
+        var windowheight = $(scroll).height(),
+            tasks = $(".batch_task", scroll),
+            margin = parseInt(tasks.css("marginBottom").replace(/px/, ""));
+        return Math.ceil(windowheight / (tasks.outerHeight() + margin));
+    }
+
+    function updateTaskData(data, from, to) {
+        var taskdata = data.fields.tasks;
+        $(".batch_task").slice(from, to).each(function(i, elem) {
+            $(elem)
+                .attr("id", "task" + taskdata[i].pk)
+                .find(".page_name")
+                .text(taskdata[i].fields.page_name)
+                .end()
+                .find(".page_info")
+                .text("Lines: " + taskdata[i].fields.lines)
+                .end()
+                .find(".retry_task")
+                .attr("href", "/ocrtasks/retry/" + taskdata[i].pk + "/")
+                .end()
+                .find(".abort_task")
+                .attr("href", "/ocrtasks/abort/" + taskdata[i].pk + "/")
+                .end()
+                .find(".progress")
+                .css("width", taskdata[i].fields.progress + "%")
+                .attr("title", taskdata[i].fields.progress + "%")
+                .end();
+                
+        });
+    }
+
+    function refreshTasks(scroll) {
+        var pk = $("#ocr_batch").data("index");
+        var first = Math.max(0, topVisibleTask(scroll) - 5)
+            count = visibleTaskCount(scroll) + 10;
+        var data = {
+            start: first,
+            limit: count,
+        };
+        $.ajax({
+            url: "/batch/results/" + pk + "/",
+            data: data,
+            beforeSend: function() {
+                $(".batch_task").slice(first, first + count)
+                    .addClass("loading");
+            },
+            success: function(data) {
+                var data = data[0];
+                console.log(data);
+                updateTaskData(data, first, first + count);
+            },
+            complete: function() {
+                $(".batch_task").removeClass("loading");
+            }
+
+        });
+    }
+
+    function triggerRefresh(scroll) {
+        if (scrolltimer != -1)
+            clearTimeout(scrolltimer);
+        scrolltimer = setTimeout(function() {
+            refreshTasks(scroll);
+        }, 100);
+    }
+
+    refreshTasks($(".tl_container").get(0));
+
+    $(".tl_container").scroll(function(event) {
+        triggerRefresh(this);            
+    });
 
     if ($("#batch_id").length) {
         var type = $("#batch_type").val();
@@ -113,17 +205,16 @@ $(function() {
                 widget = OCRJS.ExportWidget;
                 break;
             default:
-                widget = OCRJS.BatchWidget;
+                widget = OCRJS.BatchWidget2;
         }
-        console.log(widget);
-        batch = new widget($("#workspace").get(0), $("#batch_id").val());
-        batch.addListeners({
-            onTaskSelected: loadTaskDetails,
-            onTaskDeselected: function() {
-                loadBatchList();
-            },
-            onUpdate: hashNavigate,                                
-        }).init();
+        //batch = new widget($("#workspace").get(0), $("#batch_id").val());
+        //batch.addListeners({
+        //    onTaskSelected: loadTaskDetails,
+        //    onTaskDeselected: function() {
+        //        loadBatchList();
+        //    },
+        //    onUpdate: hashNavigate,                                
+        //}).init();
 
         window.addEventListener("hashchange", function() {
             //hashNavigate();
