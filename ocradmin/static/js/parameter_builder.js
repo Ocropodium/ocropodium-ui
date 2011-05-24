90e5e8db24e55147f0af54755a0f9c150d262904
mikesname <mikesname@localhost>
2011-05-24 22:42:52 +0100

    Sketched out the very basics of a node editor.

--- ocradmin/static/js/parameter_builder.js
+++ ocradmin/static/js/parameter_builder.js
@@ -56,7 +56,10 @@ OCRJS.ParameterBuilder = OCRJS.OcrBase.extend({
         this._temp = null;
         this._waiting = {};
         this._initialised = false;
-        this._nodetemplate = $.template($("#nodeTemplate"));
+        this._nodetemplate = $.template($("#nodeTmpl"));
+        this._menutemplate = $.template($("#nodeMenuTmpl"));
+        this._menu = null;
+        this._nodedata = {};
     },
 
     init: function() {
@@ -69,8 +72,116 @@ OCRJS.ParameterBuilder = OCRJS.OcrBase.extend({
     },
 
     setupEvents: function() {
+        var self = this;
+        console.log(self.parent);
+        $(self.parent).noContext().rightClick(function(event) {
+            self._menu.show();
+            var maxx = $(self.parent).offset().left + $(self.parent).width();
+            var left = event.clientX;
+            if (event.clientX + self._menu.outerWidth() > maxx)
+                left = maxx - (self._menu.outerWidth() + 20);
+            self._menu.css({
+                top: event.clientY,
+                left: left,    
+            });
+        });
+        $(self.parent).click(function(event) {
+           self._menu.hide();
+        });
+
+        $(".node.floating").live("click", function(event) {
+            $(this).removeClass("floating");
+            $(document).unbind("mousemove.dropnode");
+        });
 
     },                 
+
+    setupMenuEvents: function() {
+        var self = this;                         
+        console.log("setup menu");         
+
+        self._menu.find("li").hover(function(event) {
+            $(this).addClass("selected");
+        }, function(event) {
+            $(this).removeClass("selected");
+        });
+
+        self._menu.find("li.topmenu").hover(function(event) {
+            var pos = $(this).position();
+            var left = pos.left + $(this).outerWidth() - 5;
+            var sub = $(this).find("ul");
+            sub.show();
+            sub.css({left: left, top: $(this).position().top})
+            var span = $(this).offset().left + $(this).outerWidth() + sub.outerWidth();
+            var outer = $(self.parent).offset().left + $(self.parent).width();
+            console.log("span", span, "doc", outer);
+            if (span > outer) {
+                console.log("moving left");
+                sub.css("left", pos.left - sub.outerWidth());
+            }
+        }, function(event) {
+            $(this).find("ul").delay(1000).hide();            
+        });
+
+        self._menu.find(".topmenu").find("li").click(function(event) {
+            var node = self.createNode($(this).text());
+            node.css({
+                position: "absolute",
+                left: event.clientX - (node.width() / 2),
+                top: event.clientY - (node.height() / 2),
+            });
+            $(document).bind("mousemove.dropnode", function(e) {
+                var con = self._constrainDrag({
+                    left: e.clientX - (node.width() / 2),
+                    top: e.clientY - (node.height() / 2),
+                }, node, $(self.parent));
+                node.css({
+                    left: con.left,
+                    top: con.top,
+                });
+            });
+            $(this).hide();
+            self._menu.hide();
+            event.stopPropagation();
+            event.preventDefault();
+        });
+
+    },
+
+    _constrainDrag: function(pos, node, parent) {
+        var self = this;                        
+        var left = pos.left;
+        left = Math.max(left, parent.offset().left);
+        left = Math.min(left, (parent.offset().left + parent.width()) - node.width());
+        var top = pos.top;
+        top = Math.max(top, $(self.parent).offset().top);
+        top = Math.min(top, (parent.offset().top + parent.height()) - node.height());                
+        return {left: left, top: top};
+    },            
+
+    createNode: function(name) {
+        var self = this;
+        console.log("Creating node: ", name);
+        var nodeinfo;
+        $.each(self._nodedata, function(stage, nodes) {
+            if (!nodeinfo) {
+                for (var i = 0; i < nodes.length; i++) {
+                    if (nodes[i].name == name) {
+                        nodeinfo = nodes[i];
+                        break;
+                    }
+                }
+            }
+        });
+        return $.tmpl(self._nodetemplate, nodeinfo)
+            .draggable({
+                containment: self.parent,
+                stack: ".node",
+                cursor: "default",
+            })
+            .appendTo($(self.parent))
+            .addClass("floating");
+    },
 
     isReady: function() {
         return OCRJS.countProperties(this._waiting) > 0 ? false : true;
@@ -87,10 +198,26 @@ OCRJS.ParameterBuilder = OCRJS.OcrBase.extend({
             type: "GET",
             error: OCRJS.ajaxErrorHandler,
             success: function(data) {
-                self._temp = self.buildSection(parent, data)
+                self._nodedata = {};
+                $.each(data, function(i, nodeinfo) {
+                    if (!self._nodedata[nodeinfo.stage])
+                        self._nodedata[nodeinfo.stage] = [];
+                    self._nodedata[nodeinfo.stage].push(nodeinfo);
+                });                
+                self.buildNodeMenu();
+                console.log("Building node menu");
                 self._queryDone(parent.id);
             },
         });
+    },
+
+    buildNodeMenu: function() {
+        var self = this;
+        self._menu = $.tmpl(this._menutemplate, {
+            stages: self._nodedata,
+        }).hide();
+        $(self.parent).append(self._menu);
+        self.setupMenuEvents();
     },
 
     buildSection: function(parent, data) {
