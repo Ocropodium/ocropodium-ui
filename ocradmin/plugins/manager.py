2a8f688f2e37b9efdafaf02e58abe4ff5868c0ea
mikesname <mikesname@localhost>
2011-05-24 17:22:54 +0100

    Removed lots of dead code in preparation for trying to get things working again.

--- ocradmin/plugins/manager.py
+++ ocradmin/plugins/manager.py
@@ -37,6 +37,16 @@ class StandardManager(object):
         return nodes
                     
 
+class PluginManager(object):
+    """
+    Old version - to be removed.
+    """
+    @classmethod
+    def get_converter(cls, name):
+        pass
+
+    def get_trainer(cls, name):
+        pass
 
 
 class ModuleManager(object):
@@ -96,105 +106,4 @@ class ModuleManager(object):
         Get Node json.
         """
         return json.dumps(cls.get_nodes(*oftypes), cls=utils.NodeEncoder)
-
-
-    #@classmethod
-    #def get_provider(cls, provides=None):
-    #    """
-    #    Get a list of available OCR engines.        
-    #    """
-    #    engines = []
-    #    plugdir = os.path.join(os.path.dirname(__file__), "tools")
-    #    for fname in os.listdir(plugdir):
-    #        if fname.endswith("wrapper.py"):
-    #            modname = os.path.splitext(os.path.basename(fname))[0]
-    #            pm = __import__(
-    #                    modname,
-    #                    fromlist=["main_class"])
-    #            if not hasattr(pm, "main_class"):
-    #                continue
-    #            mod = pm.main_class()
-    #            if provides is None:
-    #                engines.append(mod.name)
-    #            elif isinstance(provides, str) and provides in mod.capabilities:
-    #                engines.append(mod.name)
-    #            elif isinstance(provides, tuple):
-    #                for cap in provides:
-    #                    if cap in mod.capabilities:
-    #                        engines.append(mod.name)
-    #                        break
-    #    return engines
-
-
-    #@classmethod
-    #def get(cls, name, *args, **kwargs):
-    #    """
-    #    Get a plugin directly.
-    #    """
-    #    return cls._main_class(name)
-
-
-    #@classmethod
-    #def get_info(cls, name, *args, **kwargs):
-    #    """
-    #    Get info about a plugin.
-    #    """
-    #    mc = cls._main_class(name)
-    #    if mc is not None:
-    #        return mc.get_info(*args, **kwargs)
-
-
-    #@classmethod
-    #def get_parameters(cls, name, *args, **kwargs):
-    #    """
-    #    Get general options for an engine.
-    #    """
-    #    print "Getting options: " + name
-    #    mc = cls._main_class(name)
-    #    if mc is not None:
-    #        return mc.get_parameters(*args, **kwargs)
-
-
-    #@classmethod
-    #def get_trainer(cls, name, *args, **kwargs):
-    #    """
-    #    Fetch a given trainer class.  Currently this is the
-    #    same as the converter.
-    #    """
-    #    return cls.get_converter(name, *args, **kwargs)
-
-
-    #@classmethod
-    #def get_converter(cls, name, *args, **kwargs):
-    #    """
-    #    Get a converter with a given name.
-    #    """
-    #    mc = cls._main_class(name)
-    #    if mc is not None:
-    #        return mc(*args, **kwargs)
-
-
-    #@classmethod
-    #def get_components(cls, name, *args, **kwargs):
-    #    """
-    #    Get available components of the given type for given plugin.
-    #    """
-    #    mc = cls._main_class(name)
-    #    if mc is not None:
-    #        return mc.get_components(*args, **kwargs)
-
-
-    #@classmethod
-    #def _main_class(cls, name):
-    #    """
-    #    Get the plugin class with a given name.
-    #    """
-    #    plugdir = os.path.join(os.path.dirname(__file__), "tools")
-    #    for fname in os.listdir(plugdir):
-    #        modname = "%s_wrapper.py" % name
-    #        if fname == modname:
-    #            # FIXME: Hard-coded module import path needs to change
-    #            # TODO: Generally find a better way of doing this...
-    #            pm = __import__("%s_wrapper" % name, fromlist=["main_class"])
-    #            return pm.main_class()
-
+    
--- ocradmin/plugins/urls.py
+++ ocradmin/plugins/urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^/?$', 'ocradmin.plugins.views.index'),
     (r'^query/?$', 'ocradmin.plugins.views.query'),
-    (r'^query/(?P<args>.+)/?$', 'ocradmin.plugins.views.query'),
-    (r'^parse/?$', 'ocradmin.plugins.views.parse'),
-	(r'^(?P<name>[^/]+)/?$', 'ocradmin.plugins.views.info'),
-	(r'^(?P<name>\w+)/(?P<method>\w+)/?$', 'ocradmin.plugins.views.run_get_method'),
 )
