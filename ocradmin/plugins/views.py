9fdd451ed93930ed98480877ec8cae6116a8f4ad
mikesname <mikesname@localhost>
2011-05-25 17:16:07 +0100

    Added function for testing POSTed scripts

--- ocradmin/plugins/views.py
+++ ocradmin/plugins/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import render_to_response
 
 from ocradmin.plugins.manager import ModuleManager 
 
+import logging
+logger = logging.getLogger(__name__)
+
+import simplejson
 
 def query(request):
     """
@@ -16,3 +20,24 @@ def query(request):
     stages=request.GET.getlist("stage")
     return HttpResponse(
             ModuleManager.get_json(*stages), mimetype="application/json")
+
+def runscript(request):
+    """
+    Execute a script (sent as JSON).
+    """
+    node = request.POST.get("node", "")
+    script = request.POST.get("script", simplejson.dumps({"arse":"spaz"}))
+    data = simplejson.loads(script)
+
+    import apply
+    try:
+        pl = apply.OcrPipeline(data)
+        term = pl.get_node(node)
+        val = term.eval()
+        print "VALUE: ", val
+        logger.debug("Val is: %s", val)
+    except StandardError, err:
+        raise
+
+
+    return HttpResponse(simplejson.dumps(val), mimetype="application/json")
