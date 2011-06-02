af0e7f6faefb5a47acee3eec764638aa8a4f1a24
mikesname <mikesname@localhost>
2011-06-02 17:33:34 +0100

    Make the caller responsible for supplying the FULL read/write path.  This is more sensible

--- ocradmin/lib/nodetree/writable_node.py
+++ ocradmin/lib/nodetree/writable_node.py
@@ -20,17 +20,15 @@ class WritableNodeMixin(object):
         return "%s%s" % (cls.name, cls.extension)
 
     @classmethod
-    def reader(cls, dirpath):
+    def reader(cls, path):
         """Read a cache from a given dir."""
-        fpath = os.path.join(dirpath, cls.get_file_name())
-        if os.path.exists(fpath):
-            with open(fpath, "r") as fh:
+        if os.path.exists(path):
+            with open(path, "r") as fh:
                 return pickle.load(fh)
 
     @classmethod
-    def writer(cls, dirpath, data):
+    def writer(cls, path, data):
         """Write a cache from a given dir."""
-        fpath = os.path.join(dirpath, cls.get_file_name())
-        with open(fpath, "w") as fh:
+        with open(path, "w") as fh:
             pickle.dump(data, fh)
-        return fpath            
+        return path            
