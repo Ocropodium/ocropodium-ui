a7ae6994907f463f25bd36c8e1194213cde032a0
mikesname <mikesname@localhost>
2011-06-02 13:18:29 +0100

    Fleshed out the generic preset management views a bit more, and moved really generic code to core/generic_views

--- ocradmin/ocrpresets/urls.py
+++ ocradmin/ocrpresets/urls.py
@@ -1,10 +1,43 @@
 
 from django.conf.urls.defaults import *
-from ocradmin.ocrpresets.views import PresetListView, PresetEditView
+#from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from ocradmin.ocrpresets import views
+from ocradmin.ocrpresets.models import OcrPreset
+from ocradmin.core import generic_views as gv
+
+
+presetlist = gv.GenericListView.as_view(
+        model=OcrPreset,
+        page_name="OCR Presets",
+        fields=["name", "description", "user", "created_on"],)
+
+presetcreate = gv.GenericCreateView.as_view(
+        model=OcrPreset,
+        form_class=views.OcrPresetForm,
+        page_name="New OCR Preset",)
+
+presetdetail = gv.GenericDetailView.as_view(
+        model=OcrPreset,
+        page_name="OCR Preset",
+        fields=["name", "description", "user", "public", "tags", "created_on",
+            "updated_on",])
+
+presetedit = gv.GenericEditView.as_view(
+        model=OcrPreset,
+        form_class=views.OcrPresetForm,
+        page_name="Edit OCR Preset",)
+
+presetdelete = gv.GenericDeleteView.as_view(
+        model=OcrPreset,
+        page_name="Delete OCR Preset",
+        success_url="/ocrpresets/list/",)
 
 urlpatterns = patterns('',
-    (r'^list/?$', PresetListView.as_view(template_name="generic_list.html")),
-    (r'^edit/(?P<object_id>\d+)/?$', PresetEditView.as_view()),
-
-
+    (r'^list/?$', presetlist),
+    (r'^create/?$', login_required(presetcreate)),
+    (r'^show/(?P<pk>\d+)/?$', presetdetail),
+    (r'^edit/(?P<pk>\d+)/?$', login_required(presetedit)),
+    (r'^delete/(?P<pk>\d+)/?$', login_required(presetdelete)),
+    (r'^data/(?P<pk>\d+)/?$', views.data),
 )
