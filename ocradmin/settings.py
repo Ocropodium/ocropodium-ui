ef8dea77f320690172952f7f08e17f1e5f34ebb9
mikesname <mikesname@localhost>
2011-06-03 14:35:50 +0100

    Fix deprecation warnings (upgraded picklefield, using the one on pypi)

--- ocradmin/settings.py
+++ ocradmin/settings.py
@@ -145,7 +145,8 @@ SECRET_KEY = 'vg@k)$%0#dn=xdelu613c6)y%yhxs)6himtf0l(i)dcpq_9jzp'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
+#    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.load_template_source',
 #     'django.template.loaders.eggs.load_template_source',
 )
