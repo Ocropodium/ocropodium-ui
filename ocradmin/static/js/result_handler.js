740ed87a18f4560970987962d182b1188eb97dd9
mikesname <mikesname@localhost>
2011-06-07 12:15:38 +0100

    Remove some debug

--- ocradmin/static/js/result_handler.js
+++ ocradmin/static/js/result_handler.js
@@ -24,7 +24,6 @@ OCRJS.ResultHandler = OCRJS.OcrBase.extend({
 
     watchNode: function(nodename, data) {
         var self = this;
-        console.log("Nodename", nodename, "Data", data);
         this._nodetasks[nodename] = data.task_id;
         this._tasknodes[data.task_id] = nodename;
         this._nodedata[nodename] = data;
@@ -37,7 +36,6 @@ OCRJS.ResultHandler = OCRJS.OcrBase.extend({
 
     pollForResults: function() {
         var self = this;
-        console.log("Polling with", self._pending);
         if (this._timer) {
             clearTimeout(this._timer);
             this._timer = null;
@@ -48,18 +46,14 @@ OCRJS.ResultHandler = OCRJS.OcrBase.extend({
                 error: OCRJS.ajaxErrorHandler,
                 success: function(ndata) {
                     $.each(ndata, function(i, data) {
-                        console.log("Data: status", data["status"], "Data", data);
                         if (data.status != "PENDING")
                             self._pending = null;
-                            
-                        
                         if (data.status == "SUCCESS") {
                             self.callListeners("resultDone", 
                                     self._tasknodes[data.task_id], data); 
                         } else if (data.status != "PENDING") {
                             console.log("Got status", data.status);
                         }
-
                     });
                     if (self._pending)
                         self.pollForResults();
