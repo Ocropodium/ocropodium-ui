8c5e95312d5273054bc69bafc97575fe0838900d
mikesname <mikesname@localhost>
2011-06-11 21:06:11 +0100

    Baby steps towards decent preset management

--- ocradmin/static/js/preset_manager.js
+++ ocradmin/static/js/preset_manager.js
@@ -11,9 +11,10 @@ OCRJS.PresetManager = OCRJS.OcrBase.extend({
     constructor: function(parent, options) {
         this.base(parent, options);
         this.parent = parent;
-        this.setupEvents();
 
         this._dialog = $("#dialog", this.parent);
+        this._updatetmpl = $.template($("#updateDialog"));
+        this._createtmpl = $.template($("#createDialog"));
 
         this._listeners = {
             saveDialogOpen: [],
@@ -21,83 +22,63 @@ OCRJS.PresetManager = OCRJS.OcrBase.extend({
         };
     },
 
-    setupEvents: function() {
+    showNewPresetDialog: function(scriptdata) {
+
         var self = this;        
+        if (this._dialog.dialog("isOpen") === true) {
+            this._dialog.dialog("close");
 
-        $("#save_script", self.parent).click(function(event) {
-            if (self._dialog.dialog("isOpen") === true) {
-                self._dialog.dialog("close");
-
-                event.stopPropagation();
-                return;
-            }
-
-            var tb = $(self.parent);        
-            var pos = [tb.offset().left, tb.offset().top + tb.height()];
-            self._dialog.load("/presets/create/", function() {
-                self._dialog.find("#id_data")
-                    .val(JSON.stringify(pbuilder.buildScript()));
-//console.log("Set form data:". self._dialog.find("#id_data").val());
-                self.handleForm();                    
-                self._dialog.dialog({
-                    dialogClass: "save_dialog",
-                    position: pos,
-                    width: tb.width(),
-                    close: function(e, ui) {
-                        self._dialog.children().remove();
-                        self.callListeners("saveDialogClose");    
-                    },
-                });
-            });
-            self.callListeners("saveDialogOpen");
             event.stopPropagation();
-            event.preventDefault();    
-        });
-    },
+            return;
+        }
 
-    validateNewForm: function() {
-        var self = this;
-        var namefield = self._dialog.find("#id_name");
-        var submit = self._dialog.find("input[type='submit']");
-        submit.attr("disabled", $.trim(namefield.val()) == "");
-    },                 
-
-    handleForm: function() {
-        var self = this;
-        var submit = $("input[type='submit']", self._dialog),
-            namefield = $("#id_name", self._dialog),
-            stockval = namefield.val(),
-            datafield = $("#id_data", self._dialog);                        
-
-        // validate the name
-        namefield.keyup(function(event) {
+        var tb = $(this.parent);        
+        var pos = [tb.offset().left, tb.offset().top + tb.height()];
+        this._dialog.html($.tmpl(this._createtmpl, {}));
+        this._dialog.find("input[type='text']").keyup(function(event) {
             self.validateNewForm();
-        });                 
-        // hide the data field and add data from the script
-        datafield
-            .parent().hide();
-
-        submit.click(function(sevent) {
+        });
+        this._dialog.find("input[type='submit']").click(function(event) {
+            var name = self._dialog.find("#id_name").val();
             $.ajax({
                 url: "/presets/create/",
                 type: "POST",
                 data: self._dialog.find("form").serialize(),
                 error: OCRJS.ajaxErrorHandler,
                 statusCode: {
-                    200: function() {
-                        self.rebuildPresetList(namefield.val());
+                    200: function(data) {
+                        // FIXME: Assume something actually worked here... if
+                        // it fails we'll get another form, but it's fiddly
+                        // to distinguish from the redirect response
                         self._dialog.dialog("close");
-                    },
-                    202: function(data) {
-                        self._dialog
-                            .html(data);
-                        self.handleForm();
-                        self.validateNewForm();
-                    },
+                        self.rebuildPresetList(name);
+                    },        
                 }
             });
-            return false;
+            event.preventDefault();
+            event.stopPropagation();
         });
+        this._dialog.find("#id_data").val(scriptdata);
+
+        this._dialog.dialog({
+            dialogClass: "save_dialog",
+            position: pos,
+            width: tb.width(),
+            close: function(e, ui) {
+                self._dialog.children().remove();
+                self.callListeners("saveDialogClose");    
+            },
+        });
+        this.callListeners("saveDialogOpen");
+        event.stopPropagation();
+        event.preventDefault();    
+    },
+
+    validateNewForm: function() {
+        console.log("validating");                         
+        var namefield = this._dialog.find("#id_name");
+        var submit = this._dialog.find("#create_new");
+        submit.attr("disabled", $.trim(namefield.val()) == "");
     },                 
 
     rebuildPresetList: function(current) {
@@ -106,7 +87,7 @@ OCRJS.PresetManager = OCRJS.OcrBase.extend({
             select.children().slice(1).remove();
             $.each(data, function(i, preset) {
                 var opt = $("<option></option>");
-                    opt.attr("value", preset.slug);
+                    opt.attr("value", preset.fields.slug);
                     opt.text(preset.fields.name);
                     opt.attr("title", preset.fields.description);
                 if (preset.fields.name == current)
