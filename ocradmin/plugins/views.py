e70a0113d0f9605a2719e16ef94a7d551d07fa82
mikesname <mikesname@localhost>
2011-06-15 11:26:20 +0100

    Cleanup prior to more refactoring.  Get and mark errors for all bad nodes when submitting script

--- ocradmin/plugins/views.py
+++ ocradmin/plugins/views.py
@@ -1,30 +1,26 @@
 """
 RESTful interface to interacting with OCR plugins.
 """
+
 import os
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render_to_response
 
-from ocradmin.ocrtasks.models import OcrTask
-from nodetree.manager import ModuleManager 
+from django.http import HttpResponse
+from django.utils import simplejson as json
+
 from ocradmin.core.decorators import saves_files
+from ocradmin.ocrtasks.models import OcrTask
+from ocradmin.plugins import graph
 
-import logging
-logger = logging.getLogger(__name__)
+from nodetree import script, node
+from nodetree.manager import ModuleManager
 
-import simplejson
-
-import tasks
-
-import numpy
-
-manager = ModuleManager()
-manager.register_module("ocradmin.plugins.ocropus_nodes")
-manager.register_module("ocradmin.plugins.tesseract_nodes")
-manager.register_module("ocradmin.plugins.cuneiform_nodes")
-manager.register_module("ocradmin.plugins.abbyy_nodes")
-manager.register_module("ocradmin.plugins.numpy_nodes")
-manager.register_module("ocradmin.plugins.pil_nodes")
+MANAGER = ModuleManager()
+MANAGER.register_module("ocradmin.plugins.ocropus_nodes")
+MANAGER.register_module("ocradmin.plugins.tesseract_nodes")
+MANAGER.register_module("ocradmin.plugins.cuneiform_nodes")
+MANAGER.register_module("ocradmin.plugins.abbyy_nodes")
+MANAGER.register_module("ocradmin.plugins.numpy_nodes")
+MANAGER.register_module("ocradmin.plugins.pil_nodes")
 
 def query(request):
     """
@@ -32,9 +28,10 @@ def query(request):
     of available OCR engines and an URL that
     can be queries when one of them is selected.
     """
-    stages=request.GET.getlist("stage")
+    stages = request.GET.getlist("stage")
     return HttpResponse(
-            manager.get_json(*stages), mimetype="application/json")
+            MANAGER.get_json(*stages), mimetype="application/json")
+
 
 @saves_files
 def runscript(request):
@@ -43,29 +40,23 @@ def runscript(request):
     """
     evalnode = request.POST.get("node", "")
     jsondata = request.POST.get("script")
-    nodes = simplejson.loads(jsondata)
-    
-
-    from nodetree import script, node
-
-    try:
-        pl = script.Script(nodes, manager=manager)
-        term = pl.get_node(evalnode)
-        if term is None:
-            terms = pl.get_terminals()
-            if not terms:
-                return HttpResponse(simplejson.dumps(dict(
-                    status="NOSCRIPT",
-                )), mimetype="application/json")
-            term = terms[0]                
-        result = term.validate()
-    except node.ValidationError, err:
-        return HttpResponse(simplejson.dumps(dict(
+    nodes = json.loads(jsondata)
+    tree = script.Script(nodes, manager=MANAGER)
+    errors = tree.validate()
+    if errors:
+        return HttpResponse(json.dumps(dict(
             status="VALIDATION",
-            node=err.node.label,
-            error=err.msg,
+            errors=errors,
         )), mimetype="application/json")
 
+    term = tree.get_node(evalnode)
+    if term is None:
+        terms = tree.get_terminals()
+        if not terms:
+            return HttpResponse(json.dumps(dict(
+                status="NOSCRIPT",
+            )), mimetype="application/json")
+        term = terms[0]
     async = OcrTask.run_celery_task("run.script", evalnode, nodes,
             request.output_path, untracked=True,
             asyncronous=True, queue="interactive")
@@ -75,23 +66,22 @@ def runscript(request):
         status=async.status,
         results=async.result
     )
-    return HttpResponse(simplejson.dumps(out), mimetype="application/json")
+    return HttpResponse(json.dumps(out), mimetype="application/json")
 
 
 def results(request, task_ids):
     """
     Fetch the results of several Celery task ids.
     """
-    results = []
+    out = []
     for task_id in task_ids.split(","):
         async = OcrTask.get_celery_result(task_id)
-        results.append(dict(
+        out.append(dict(
             result=async.result,
             task_id=task_id,
-            status=async.status,            
+            status=async.status,
         ))
-
-    return HttpResponse(simplejson.dumps(results), mimetype="application/json")
+    return HttpResponse(json.dumps(out), mimetype="application/json")
 
 
 @saves_files
@@ -99,8 +89,7 @@ def upload_file(request):
     """
     Upload a temp file.
     """
-    import os
-    fpath = os.path.join(request.output_path, 
+    fpath = os.path.join(request.output_path,
             request.GET.get("inlinefile"))
     if not os.path.exists(request.output_path):
         os.makedirs(request.output_path, 0777)
@@ -108,7 +97,7 @@ def upload_file(request):
     tmpfile.write(request.raw_post_data)
     tmpfile.close()
 
-    return HttpResponse(simplejson.dumps(dict(
+    return HttpResponse(json.dumps(dict(
         file=os.path.relpath(fpath),
     )), mimetype="application/json")
 
@@ -117,10 +106,9 @@ def layout_graph(request):
     """
     Get GraphViz positions for nodes in a list.
     """
-    from ocradmin.plugins import graph
     jsonscript = request.POST.get("script")
-    print "SCRIPT: ", jsonscript
-    nodes = simplejson.loads(jsonscript)
+    nodes = json.loads(jsonscript)
     return HttpResponse(
-            simplejson.dumps(graph.get_node_positions(nodes)),
+            json.dumps(graph.get_node_positions(nodes)),
                 mimetype="application/json")
+
