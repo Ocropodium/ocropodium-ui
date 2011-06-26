a754323facdb05b18d19a1a0365ad12e8c25ed06
mikesname <mikesname@localhost>
2011-06-26 12:19:04 +0100

    Test the presence of various tools

--- ocradmin/core/tests/test_core.py
+++ ocradmin/core/tests/test_core.py
@@ -22,7 +22,32 @@ class CoreTest(TestCase):
         in usage info.  Basically we want to make sure
         that the accuracy binary is available.
         """
-        p = sp.Popen(["accuracy"], stderr=sp.PIPE)
-        self.assertRegexpMatches(p.communicate()[1], "^Usage")
+        stdout, stderr = self._run_cmd("accuracy")
+        self.assertRegexpMatches(stderr, "^Usage")
 
+    def test_cuneiform(self):
+        """
+        Ensure cuneiform is available.  This is fragile since it depends
+        on Cuneiform's annoying output on stdout.
+        """
+        stdout, stderr = self._run_cmd("cuneiform")
+        self.assertRegexpMatches(stdout, "^Cuneiform for Linux")
+
+    def test_tesseract(self):
+        """
+        Ensure tesseract is available.
+        """
+        stdout, stderr = self._run_cmd("tesseract")
+        self.assertRegexpMatches(stderr, "^Usage")
+
+    def test_convert(self):
+        """
+        Ensure (Image|Graphics)Magick is available.
+        """
+        stdout, stderr = self._run_cmd("convert")
+        self.assertRegexpMatches(stdout, "Usage")
+
+    def _run_cmd(self, *args):
+        p = sp.Popen(args, stdout=sp.PIPE, stderr=sp.PIPE)
+        return p.communicate()
 
