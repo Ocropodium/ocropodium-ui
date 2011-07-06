b80865ca9461ad826760646110e8fe86c1268afe
mikesname <mikesname@localhost>
2011-07-06 09:11:51 +0100

    Add file forgotten in last night's commits

---
+++ ocradmin/static/js/status_bar.js
@@ -0,0 +1,53 @@
+//
+// Status bar widget
+//
+
+var OCRJS = OCRJS || {};
+
+OCRJS.StatusBar = OCRJS.OcrBase.extend({
+    constructor: function(parent, options) {
+        this.parent = parent;
+        this._default = "Welcome to OWP";
+
+        this._left = $("#status_message1", parent);
+        this._right = $("#status_message2", parent);
+        this._cancel = $("#progress_bar_cancel", parent);
+
+        this._listeners = {
+            messageChanged: [],
+            progressChanged: [],
+            cancel: [],
+        };
+
+        var self = this;
+        this._cancel.bind("click", function(event) {
+            self.callListeners("cancel");            
+        }).bind("mouseenter", function(event) {
+            $(this).addClass("hover");    
+        }).bind("mouseleave", function(event) {
+            $(this).removeClass("hover");    
+        });            
+    },
+
+    setStatus: function(string) {
+        this._left.text(string).css({
+            color: "#333",
+        });                    
+    },
+
+    clearStatus: function() {
+        this._left.text(this._default).css(
+            "color", this._left.css("backgroundColor"));        
+    },                  
+
+    setProgress: function(percent) {
+        
+    },
+
+    setWorking: function(bool) {
+        this._right.css({
+            visibility: bool ? "visible" : "hidden",
+        });
+    },                    
+
+});
