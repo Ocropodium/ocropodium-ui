fe8b4d8ded35ee3aba2faf0e5831ae73209793b2
mikesname <mikesname@localhost>
2011-09-22 20:58:36 +0100

    Added a class decorator to handle boilerplate

--- ocradmin/lib/nodetree/decorators.py
+++ ocradmin/lib/nodetree/decorators.py
@@ -23,9 +23,10 @@ def upper_camelcase(value):
 class makenode(object):
     """Decorate for constructing a node out
     of a single function."""
-    def __init__(self, intypes, outtype, **kwargs):
-        self.intypes = intypes
-        self.outtype = outtype
+    def __init__(self, *args, **kwargs):
+        assert len(args) > 0, "Nodes must have an output type"
+        self.intypes = args[:-1]
+        self.outtype = args[-1]
         self.kwargs = kwargs
 
     def __call__(self, fun):
@@ -52,4 +53,37 @@ class makenode(object):
         clsdict.update(self.kwargs)
         return type(clsname + "Node", (node.Node,), clsdict)()
 
+
+class Node(object):
+    """Decorator to turn nodes into classes."""
+    def __init__(self, *args, **kwargs):
+        assert len(args) > 0, "Nodes must have an output type"
+        self.intypes = args[:-1]
+        self.outtype = args[-1]
+        self.kwargs = kwargs
+
+    def __call__(self, cls):
+        argspec = inspect.getargspec(cls.run)
+
+        def _eval(self):
+            args = [self.eval_input(i) for i in range(1, len(argspec.args))]
+            return self.run(*args)
+
+        doc = cls.__doc__ if not cls.__doc__ is None \
+                else ""
+        clsname = upper_camelcase(cls.__name__)
+        ns = upper_camelcase(cls.__module__.split(".")[-1])
+        clsdict = dict(**cls.__dict__)
+        clsdict.update(
+            __module__ = cls.__module__,
+            __doc__ = doc,
+            _eval = _eval,
+            arity = len(self.intypes),
+            intypes = self.intypes,
+            outtype = self.outtype,
+            description = textwrap.dedent(doc).strip(),
+            name = "%s::%s" % (ns, clsname),
+        )
+        clsdict.update(self.kwargs)
+        return type(clsname, cls.__bases__, clsdict)
 
