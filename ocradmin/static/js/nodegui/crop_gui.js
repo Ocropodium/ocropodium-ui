401c737021acb912a9b9a7f7764b4727ff992219
mikesname <mikesname@localhost>
2011-10-21 23:21:55 +0100

    Implemented crop node with new viewer plugin system

--- ocradmin/static/js/nodegui/crop_gui.js
+++ ocradmin/static/js/nodegui/crop_gui.js
@@ -5,94 +5,128 @@
 var OCRJS = OCRJS || {};
 OCRJS.NodeGui = OCRJS.NodeGui || {}
 
-OCRJS.NodeGui.CropGui = OCRJS.NodeGui.BaseGui.extend({
-    constructor: function(viewer) {
-        this.base(viewer, "cropgui");
+OCRJS.NodeGui.PilCropGui = DziViewer.Plugin.RectManager.extend({
+    init: function(viewport, node) {
+        this._super(viewport);
+        this.node = node;
 
-        this.nodeclass = "pil.PilCrop";
-        this._coords = {
-            x0: -1,
-            y0: -1,
-            x1: -1,
-            y1: -1,
-        };
-        this.registerListener("onCanvasChanged");
-        this._node = null;
+        this._coords = new DziViewer.Rect(-1,-1,-1,-1);
 
+        this._rects = [];
+        this._outline = null;
         this._color = "#FFBBBB";
-        this._css = {
-            borderColor: "red",
-            borderWidth: 0,
-            borderStyle: "solid",                    
-            zIndex: 201,
-            backgroundColor: this._color,
-            opacity: 0.3,
-        };
-        this.setCanvasDraggable();        
+
+        this.registerListener("parametersSet");
+
+        this.readNodeData();
     },
 
-    readNodeData: function(node) {
+    refresh: function() {
+        this.readNodeData();
+        this.trigger("update");
+    },                 
+
+    render: function(context) {
+        context.save();
+
+        context.clearRect(0, 0, this.viewport.width, this.viewport.height);
+        context.fillStyle = "rgba(0,0,0,0.5)";
+        context.fillRect(0, 0, this.viewport.width, this.viewport.height);
+        context.strokeStyle = "rgba(15,80,120,0.5)";
+        context.lineWidth = 10;
+        context.strokeRect(0, 0, this.viewport.width, this.viewport.height);
+
+        for (var i in this._rects) {
+            var r = this._rects[i];
+            context.clearRect(
+                (r.x * this.viewport.scale) + this.viewport.translate.x,
+                (r.y * this.viewport.scale) + this.viewport.translate.y, 
+                r.width * this.viewport.scale, r.height * this.viewport.scale);
+        }
+
+        // draw the drag box if it exists...
+        if (this._outline !== null) {
+            context.lineWidth = 2;
+            context.strokeStyle = "#000";
+            context.fillStyle = "transparent";
+            context.dashedRect(
+                (this._outline.x * this.viewport.scale) + this.viewport.translate.x,
+                (this._outline.y * this.viewport.scale) + this.viewport.translate.y, 
+                this._outline.width * this.viewport.scale,
+                this._outline.height * this.viewport.scale);
+        }
+        context.restore();
+    },
+
+    readNodeData: function() {
+        console.log("Reading node data");
         var coords = this._coords;                      
-        $.each(node.parameters, function(i, param) {
+        $.each(this.node.parameters, function(i, param) {
             if (coords[param.name])
                 coords[param.name] = parseInt(param.value);            
         });
-        return coords;
+
+        this._rects = [coords];
     },                      
 
-    sanitiseInputCoords: function(coords) {
-        var fulldoc = this.sourceDimensions();
-        return {
-            x0: Math.max(0, Math.min(fulldoc.x, coords.x0)),
-            y0: Math.max(0, Math.min(fulldoc.y, coords.y0)),
-            x1: (coords.x1 < 0 ? fulldoc.x : Math.min(fulldoc.x, coords.x1)),
-            y1: (coords.y1 < 0 ? fulldoc.y : Math.min(fulldoc.y, coords.y1)),
-        }
-    },
-
-    sanitiseOutputCoords: function(coords) {
-        var fulldoc = this.sourceDimensions();
-        return {
-            x0: Math.round(Math.max(-1, Math.min(coords.x0, fulldoc.x))),
-            y0: Math.round(Math.max(-1, Math.min(coords.y0, fulldoc.y))),
-            x1: Math.round(Math.max(-1, Math.min(coords.x1, fulldoc.x))),
-            y1: Math.round(Math.max(-1, Math.min(coords.y1, fulldoc.y))),
-        }
-    },
-
-    draggedRect: function(rect) {
-        this.updateElement(this._rect, rect);
-        this.updateNodeParameters(rect);
-    },                     
-
-    setup: function(node) {                       
-        if (this._node)
-            return;        
-        this.base();
-        var self = this;
-        this._node = node;        
-        console.log("Setting up crop GUI");
-        var rect = this.readNodeData(node);
-        this._rect = this.addTransformableRect(rect, this._css, function(newpos) {
-            self.updateNodeParameters(newpos); 
-        });
-    },
-
     update: function() {
-        if (!this._node)
-            console.error("Gui update called with no current node");
-        this.updateTransformableRect(this._rect, this.readNodeData(this._node));
-    },                
+        this.updateNodeParameters();
+        this.trigger("update");
+    },
 
-    tearDown: function() {
-        this.base();                  
-        console.log("Tearing down crop gui");                  
-        this.removeTransformableRect(this._rect);
-        this._rect = null;        
-        this._node = null;
-    },                  
+    handleClick: function(event) {
+        if (this._super(event)) {
+            return true;
+        }
 
-    updateNodeParameters: function(srcrect) {
-        this.callListeners("parametersSet", this._node, this.sanitiseOutputCoords(srcrect));
+        if (event.button != 0)
+            return false;
+
+        this.trigger("interactingStart");
+
+        var self = this;
+        // we haven't handled a rect, so draw one
+        var spoint = this.viewport.getContentPosition(
+                new DziViewer.Point(event.pageX, event.pageY));
+        $(document).bind("mousemove.drawoutline", function(mevent) {
+            if (Math.sqrt(
+                    (Math.abs(mevent.pageX - event.pageX)^2
+                        + Math.abs(mevent.pageY - event.pageY)^2)) > 5) {
+                var cpoint = self.viewport.getContentPosition(
+                        new DziViewer.Point(mevent.pageX, mevent.pageY));
+                if (self._outline === null)
+                    self._outline = new DziViewer.Rect(spoint, cpoint).normalize();
+                self._outline.x0 = Math.min(spoint.x, cpoint.x);
+                self._outline.y0 = Math.min(spoint.y, cpoint.y);
+                self._outline.x1 = Math.max(spoint.x, cpoint.x);
+                self._outline.y1 = Math.max(spoint.y, cpoint.y);
+                self.update();
+            }
+        });
+        $(document).bind("mouseup.drawoutline", function(uevent) {
+            $(document).unbind(".drawoutline");
+            if (self._outline !== null && self._outline.area() > 50)
+                self._rects[0] = self._outline.normalize().clone();
+            self._outline = null;
+
+            // NB: The order of these operations is important
+            // if we stop interaction before update(), the
+            // GUI will be forced to update from the existing
+            // node data and the rect values get overwritten
+            self.update();
+            self.trigger("interactingStop");
+        });        
+
+        return true;
+    },
+
+    updateNodeParameters: function() {
+        var self = this;
+        this.trigger("parametersSet", this.node, {
+            x0: Math.round(self._rects[0].x0),        
+            y0: Math.round(self._rects[0].y0),        
+            x1: Math.round(self._rects[0].x1),        
+            y1: Math.round(self._rects[0].y1),        
+        });
     },                             
 });
