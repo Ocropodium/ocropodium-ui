bad352a42d4f48c0fb23e9725eb19d02534dc4b5
mikesname <mikesname@localhost>
2011-11-14 14:58:03 +0000

    Add a Fedora node package and a node to read an image from a fedora datastream

--- ocradmin/plugins/nodes/__init__.py
+++ ocradmin/plugins/nodes/__init__.py
@@ -2,4 +2,5 @@
 from __future__ import absolute_import
 
 from . import ( abbyy, ocropus, tesseract, util,
-                numpy, pil, ocrlab, cuneiform, web)
+                numpy, pil, ocrlab, cuneiform, web,
+                fedora)
