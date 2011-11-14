72b4d36a881d1a9c71d5447ec9ff364a6e099041
mikesname <mikesname@localhost>
2011-11-14 18:18:38 +0000

    Started an image datastream ingest node, although the ingest currently fails

--- ocradmin/plugins/nodes/fedora.py
+++ ocradmin/plugins/nodes/fedora.py
@@ -2,23 +2,23 @@
 Nodes for interoperating with Fedora Commons.
 """
 
+import os
 import ocrolib
 from nodetree import node, exceptions
 from cStringIO import StringIO
 
-from . import base
+from . import base, util as utilnodes
 from .. import stages, utils
 
 from eulfedora.server import Repository
+from eulfedora.models import DigitalObject, FileDatastream
 from eulfedora.util import RequestFailed
 from PIL import Image
 
 
-class FedoraImageFilein(base.ImageGeneratorNode,
-            base.GrayPngWriterMixin):
-    stage = stages.INPUT
-    intypes = []
-    outtype = ocrolib.numpy.ndarray
+class FedoraIOMixin(object):
+    """Common Mixin for objects that require access to a
+    Fedora Repository."""
     parameters = [
             dict(name="url", value="http://localhost:8080/fedora/"),
             dict(name="username", value="fedoraAdmin"),
@@ -28,7 +28,7 @@ class FedoraImageFilein(base.ImageGeneratorNode,
     ]
 
     def validate(self):
-        super(FedoraImageFilein, self).validate()
+        super(FedoraIOMixin, self).validate()
         missing = []
         for pname in [p["name"] for p in self.parameters]:
             if not self._params.get(pname, "").strip():
@@ -36,6 +36,25 @@ class FedoraImageFilein(base.ImageGeneratorNode,
         if missing:
             raise exceptions.ValidationError(
                     "Missing parameter(s): %s" % ", ".join(missing), self)
+
+
+
+def get_fedora_proxy_class(dsid):
+    fcm = "info:fedora/genrepo:File-1.0"
+    return type("FedoraProxyObject", (DigitalObject,), dict(
+        FILE_CONTENT_MODEL=fcm,
+        CONTENT_MODELS=[fcm],
+        DATASTREAM=FileDatastream(dsid, "Binary datastream", defaults={
+            "versionable": True,
+        })
+    ))
+
+
+class FedoraImageIn(FedoraIOMixin, base.ImageGeneratorNode,
+            base.GrayPngWriterMixin):
+    stage = stages.INPUT
+    intypes = []
+    outtype = ocrolib.numpy.ndarray
 
     def process(self):
         repo = Repository(self._params.get("url"), self._params.get("username"),
@@ -52,4 +71,55 @@ class FedoraImageFilein(base.ImageGeneratorNode,
             raise exceptions.NodeError(
                     "Error communicating with Fedora Repository: 404", self)
         return ocrolib.numpy.asarray(pil)
+
+
+class FedoraImageOut(FedoraIOMixin, utilnodes.FileOut):
+    """
+    Update/Input an image datastream to a Fedora Object
+    """
+    stage = stages.OUTPUT
+    outtype = type(None)
+
+    parameters = FedoraIOMixin.parameters + [
+        dict(name="format", value="png", choices=[
+            "png", "jpeg", "tif", "gif"
+        ]),
+    ]
+
+    def validate(self):
+        missing = []
+        for pname in [p["name"] for p in self.parameters]:
+            if not self._params.get(pname, "").strip():
+                missing.append(pname)
+        if missing:
+            raise exceptions.ValidationError(
+                    "Missing parameter(s): %s" % ", ".join(missing), self)
+
+    def process(self, input):
+        """
+        Write the input to the given path.
+        """
+        if input is None:
+            return
+        #if not os.environ.get("NODETREE_WRITE_FILEOUT"):
+        #    return input
+
+        repo = Repository(self._params.get("url"), self._params.get("username"),
+                self._params.get("password"))
+        try:
+            buf = StringIO()
+            Image.fromarray(input).save(buf, self._params.get("format").upper())
+        except IOError:
+            raise exceptions.NodeError(
+                    "Error obtaining image buffer in format: %s" % 
+                        self._params.get("format").upper(), self)
+
+        pclass = get_fedora_proxy_class(self._params.get("dsid"))
+        obj = repo.get_object(self._params.get("pid"), type=pclass)
+        obj.DATASTREAM.content = buf
+        obj.DATASTREAM.label = "Test Ingest Datastream 1"
+        obj.DATASTREAM.mimetype = "image/%s" % self._params.get("format")
+        obj.save()
+        return input
+
 
