f9de51e27255dadc51be4f8bddcaf5233b41d2c4
mikesname <mikesname@localhost>
2011-11-27 23:19:30 +0000

    Remove erroneous double-import

--- ocradmin/projects/urls.py
+++ ocradmin/projects/urls.py
@@ -2,7 +2,6 @@
 URLConf for OCR projects.
 """
 from django.conf.urls.defaults import *
-from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
 from ocradmin.projects import views
 
