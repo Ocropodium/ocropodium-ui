6561d038dca35112f6325b6b2dc9ff06d2d3fd2e
mikesname <mikesname@localhost>
2011-12-02 13:39:39 +0000

    Add a set of document ocr status flags.  Not sure if this is the best place to put this.

---
+++ ocradmin/documents/status.py
@@ -0,0 +1,17 @@
+"""
+Flags indicating document status
+"""
+
+PRETTY_STATUS = {
+        "initial" : "Initial",
+        "error" : "Error",
+        "uncorrected": "Uncorrected",
+        "part_corrected": "Part Corrected",
+        "complete" : "Complete",
+}
+
+INITIAL = "initial"         
+ERROR = "error"         
+UNCORRECTED = "uncorrected" 
+PART_CORRECTED = "part_corrected" 
+COMPLETE = "complete"
